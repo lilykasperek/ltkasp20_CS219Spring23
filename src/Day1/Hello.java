d9741022ebb9c57fa6effc2504c6d4e5de7c63e6
Lily Kasperek <ltkasp20@stlawu.edu>
2023-01-19 11:37:46 -0500

    We updated after messing around with Java syntax.

--- src/Day1/Hello.java
+++ src/Day1/Hello.java
@@ -1,8 +1,11 @@
 package Day1;
 
+// All code is organized in one or more classes.
+// Exactly one of those class must contain a main function
 public class Hello {
 
     // starting point for the code to run
+    // main function:
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
