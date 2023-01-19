50cc792131ef9688414d7fd7a099499a2cf628da
Lily Kasperek <ltkasp20@stlawu.edu>
2023-01-19 11:16:17 -0500

    Our first GitHub commit for Hello World.

---
+++ src/Day1/Hello.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class Hello {
+
+    // starting point for the code to run
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
