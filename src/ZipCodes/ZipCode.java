9cb2feabfe6a04554247a1dc880e8b0a0bc5ee6d
Lily Kasperek <ltkasp20@stlawu.edu>
2023-04-13 11:32:57 -0400

    ZipCodes.

--- src/ZipCodes/ZipCode.java
+++ src/ZipCodes/ZipCode.java
@@ -55,5 +55,7 @@ public class ZipCode {
         return this.coord.dist(that.coord);
     }
 
+
+
 }
 
--- src/ZipCodes/ZipCodeDB.java
+++ src/ZipCodes/ZipCodeDB.java
@@ -26,15 +26,31 @@ public class ZipCodeDB {
             String line = s.nextLine();
             String [] parts = line.split(",");
             ZipCode code = new ZipCode(
-                    parts[1],
-                    parts[2],
-                    parts[3],
+                    parts[1].substring(1, parts[1].length() - 1),
+                    parts[2].substring(1, parts[2].length() - 1),
+                    parts[3].substring(1, parts[3].length() - 1),
                     Double.parseDouble(parts[4]),
                     Double.parseDouble(parts[5])
             );
-
             codes.add(code);
         }
 
     }
+
+    public ZipCode findByZip(String code) {
+        for (ZipCode zc : codes)
+            if (zc.getCode().equals(code))
+                return zc;
+        return null;
+    }
+
+    public void findDuplicates() {
+        for (int i = 0; i < codes.size() - 1; i++)
+            for (int j = i + 1; j < codes.size(); j++)
+                if (codes.get(i).getCode().equals(codes.get(j).getCode())) {
+                    System.out.println(codes.get(i));
+                    System.out.println(codes.get(j));
+                }
+    }
+
 }
